6a409801a0644365dd0e91872752854b917ece4c
Tucker <Hitchiker42@gmail.com>
2013-10-06 20:15:14 -0400

    added debug.h, forgot to before

---
+++ debug.h
@@ -0,0 +1,33 @@
+#include <execinfo.h>
+#if defined (HERE_ON) && !(defined (HERE_OFF))\
+  || (defined (DEBUG)) && !(defined (NDEBUG))
+#define HERE() fprintf(stderr,"here at %s,line %d\n",__FILE__,__LINE__)
+#define HERE_MSG(string) fprintf(stderr,"here at %s,line %d\n%s\n"\
+                                 ,__FILE__,__LINE__,string)
+#define PRINT_MSG(string) fputs(string,stderr);fputs("\n",stderr)
+#define PRINT_FMT(string,fmt...) fprintf(stderr,string,##fmt);fputs("\n",stderr)
+#else
+#define HERE()
+#define HERE_MSG(string)
+#define PRINT_MSG(string)
+#define PRINT_FMT(string,fmt...)
+#endif
+#if defined (VERBOSE_LEXING) && !(defined (QUIET_LEXING))
+#define LEX_MSG(string) fputs(string,stderr);fputs("\n",stderr)
+#define LEX_FMT(string,fmt...) fprintf(stderr,string,##fmt);fputs("\n",stderr)
+#else
+#define LEX_MSG(string)
+#define LEX_FMT(string,fmt...)
+#endif
+static void print_trace(void){ 
+  void *array[50];
+  size_t size,i;
+  char** strings;
+  size = backtrace(array,50);
+  strings = backtrace_symbols(array,size);
+  printf("obtained %zd stack frames. \n", size);
+  for(i=0;i<size;i++){
+    printf("%s\n",strings[i]);
+  }
+  free(strings);
+}
